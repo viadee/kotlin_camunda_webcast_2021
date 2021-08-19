af7c3676333f6c187322815f8433d2fba49b24e8
MeP <pascal.meier@viadee.de>
2021-08-19 13:44:49 +0200

    Migrate mapper

--- kotlin-camunda-webcast-kotlin/src/main/kotlin/de/viadee/camunda/demo/mapper/ServiceCallMapper.kt
+++ kotlin-camunda-webcast-kotlin/src/main/kotlin/de/viadee/camunda/demo/mapper/ServiceCallMapper.kt
@@ -1,23 +1,23 @@
-package de.viadee.camunda.demo.mapper;
+package de.viadee.camunda.demo.mapper
 
-import de.viadee.camunda.demo.context.ExternalContext;
-import de.viadee.camunda.demo.dto.CustomerDTO;
-import org.mapstruct.*;
+import de.viadee.camunda.demo.context.ExternalContext
+import de.viadee.camunda.demo.dto.CustomerDTO
+import org.mapstruct.*
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR, componentModel = "spring")
-public interface ServiceCallMapper {
-    @Mappings({
-            @Mapping(source = "street", target = "address.street"),
-            @Mapping(source = "zipCode", target = "address.zipCode"),
-            @Mapping(source = "city", target = "address.city"),
-            @Mapping(source = "country", target = "address.country")
-    })
-    CustomerDTO map(final ExternalContext externalContext);
+interface ServiceCallMapper {
+    @Mappings(
+        Mapping(source = "street", target = "address.street"),
+        Mapping(source = "zipCode", target = "address.zipCode"),
+        Mapping(source = "city", target = "address.city"),
+        Mapping(source = "country", target = "address.country")
+    )
+    fun map(externalContext: ExternalContext?): CustomerDTO?
 
     @InheritInverseConfiguration
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    @Mappings({
-            @Mapping(target = "customerId", ignore = true)
-    })
-    void map(@MappingTarget final ExternalContext externalContext, final CustomerDTO customer);
+    @Mappings(
+        Mapping(target = "customerId", ignore = true)
+    )
+    fun map(@MappingTarget externalContext: ExternalContext?, customer: CustomerDTO?)
 }
